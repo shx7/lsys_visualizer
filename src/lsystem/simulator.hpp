78cb2c3d0d51760ef714c31ca23b6e74fdec56b4
Lyblinskii Alexander <lyblinskii.shx@gmail.com>
2016-03-16 11:57:42 +0300

    Removed util typedefs from LSystem simulator to grammar_util.hpp

--- src/lsystem/simulator.hpp
+++ src/lsystem/simulator.hpp
@@ -37,64 +37,10 @@
 #include <tuple>
 
 #include "generator.hpp"
+#include "grammar_util.hpp"
 
 namespace lsystem
 {
-    struct Parameter
-    {
-        std::string name = "";
-        GLfloat value = 0;
-
-        Parameter(std::string const &name, GLfloat value)
-            : name(name)
-            , value(value)
-        {}
-    };
-
-    struct Symbol
-    {
-        std::string value;
-        std::vector< Parameter > parameters;
-
-        Symbol(std::string const &value)
-            : value(value)
-        {}
-
-        void addParameter(Parameter const &param)
-        {
-            parameters.push_back(param);
-        }
-    };
-
-    typedef std::vector< Symbol > Symbols; 
-
-    typedef void (*ProducingFunction)(
-              Symbol const &symbol
-            , Symbols const &productionSymbols);
-
-    struct Production
-    {
-        std::string symbolName;
-        double probability;
-        Symbols productionSymbols;
-        ProducingFunction producingFunction;
-
-        Production(
-                  std::string const &symbolName
-                , double probability)
-            : symbolName(symbolName)
-            , probability(probability)
-        {}
-
-        void setProductionFunctoin(
-                ProducingFunction const &function)
-        {
-            producingFunction = function;
-        }
-    };
-
-    typedef std::vector< Production > Productions;
-
     class RandomGenerator
     {
         public:
