8b50fbc2f643c8ba76ff9ea4cdfc42f04ccdd298
Lyblinskii Alexander <lyblinskii.shx@gmail.com>
2016-05-30 11:32:55 +0300

    Added functional library symbols. Added new examples

--- src/simulator/main.cpp
+++ src/simulator/main.cpp
@@ -376,6 +376,209 @@ void simpleTree3()
     engine.start();
 }*/
 
+void simpleTree1b()
+{
+    std::string vertexShaderFilename("../src/shaders/vertex_shader.vert");
+    std::string fragmentShaderFilename("../src/shaders/fragment_shader.frag");
+    GraphicEngine engine;
+    engine.init("logfile", vertexShaderFilename, fragmentShaderFilename);
+
+    lsystem::Simulator &simulator = lsystem::Simulator::getInstance();
+    simulator.setStartPoint(glm::vec3(0, 0, 0));
+    simulator.setHead(glm::vec3(0, 1, 0));
+    simulator.setUp(glm::vec3(0, 0, -1));
+    simulator.setLeft(glm::vec3(-1, 0, 0));
+
+    simulator.setStartPoint(glm::vec3(0, 0, 0));
+    simulator.setDeltaAngle(glm::radians(20.0));
+    simulator.setStartAngle(glm::half_pi< GLfloat >());
+
+    lsystem::VertexGenerator &generator = lsystem::VertexGenerator::getInstance();
+
+    // Offspring
+    lsystem::Symbol symbolF =
+        lsystem::SymbolBuilder::getBuilder()
+        .setDrawingFunction(
+            [&] (lsystem::Symbol const &)
+            {
+                generator.drawLine();
+            })
+        .setProduction(
+            [=] (lsystem::Symbol const &s) -> lsystem::Symbols
+            {
+                lsystem::Symbol tmp(s);
+                lsystem::Symbols result;
+                result.push_back(tmp);
+                result.push_back(tmp);
+                return result;
+            })
+        .build();
+
+    // Main symbol
+    lsystem::Symbol symbolX =
+        lsystem::SymbolBuilder::getBuilder()
+        .setDrawingFunction(
+            [&] (lsystem::Symbol const &)
+            {
+                generator.drawLine();
+
+            })
+        .setProduction(
+            [=] (lsystem::Symbol const &s) -> lsystem::Symbols
+            {
+                lsystem::Symbol tmp(s);
+                lsystem::Symbols result;
+
+                result.push_back(symbolF);
+
+                result.push_back(lsystem::symbolSaveState);
+                result.push_back(lsystem::getSymbolYawLeft(
+                            glm::radians(20.0),
+                            [](double param) -> double
+                            {
+                                return param * 0.85;
+                            }, 0.8)
+                        );
+                result.push_back(tmp);
+                result.push_back(lsystem::symbolRestoreState);
+
+                result.push_back(lsystem::symbolSaveState);
+                result.push_back(lsystem::getSymbolPitchUp(
+                            glm::radians(20.0),
+                            [](double param) -> double
+                            {
+                                return param * 0.95;
+                            }, 0.8)
+                        );
+                result.push_back(tmp);
+                result.push_back(lsystem::symbolRestoreState);
+
+                result.push_back(symbolF);
+                result.push_back(lsystem::symbolSaveState);
+                result.push_back(
+                        lsystem::getSymbolYawRight(glm::radians(20.0), 0.0, 0.7));
+                result.push_back(tmp);
+                result.push_back(lsystem::symbolRestoreState);
+
+                result.push_back(lsystem::symbolSaveState);
+                result.push_back(lsystem::symbolPitchDown);
+                result.push_back(tmp);
+                result.push_back(lsystem::symbolRestoreState);
+
+                result.push_back(lsystem::symbolYawLeft);
+                result.push_back(tmp);
+                return result;
+            }, 0.95)
+        .build();
+
+    lsystem::Symbols axiom;
+    axiom.push_back(symbolX);
+    simulator.setAxiom(axiom);
+
+    simulator.setStepCount(4);
+    engine.addGraphicObject(
+            simulator.getGraphicObject(generator, 640 * 0.8, 480 * 0.8));
+
+
+    std::cout << "LSystem" << std::endl;
+    engine.start();
+}
+
+void simpleTree2b()
+{
+    std::string vertexShaderFilename("../src/shaders/vertex_shader.vert");
+    std::string fragmentShaderFilename("../src/shaders/fragment_shader.frag");
+    GraphicEngine engine;
+    engine.init("logfile", vertexShaderFilename, fragmentShaderFilename);
+
+    lsystem::Simulator &simulator = lsystem::Simulator::getInstance();
+    simulator.setStartPoint(glm::vec3(0, 0, 0));
+    simulator.setHead(glm::vec3(0, 1, 0));
+    simulator.setUp(glm::vec3(0, 0, -1));
+    simulator.setLeft(glm::vec3(-1, 0, 0));
+
+    simulator.setStartPoint(glm::vec3(0, 0, 0));
+    simulator.setDeltaAngle(glm::radians(20.0));
+    simulator.setStartAngle(glm::half_pi< GLfloat >());
+
+    lsystem::VertexGenerator &generator = lsystem::VertexGenerator::getInstance();
+
+    // Offspring
+    lsystem::Symbol symbolF =
+        lsystem::SymbolBuilder::getBuilder()
+        .setDrawingFunction(
+            [&] (lsystem::Symbol const &)
+            {
+                generator.drawLine();
+            })
+        .setProduction(
+            [=] (lsystem::Symbol const &s) -> lsystem::Symbols
+            {
+                lsystem::Symbol tmp(s);
+                lsystem::Symbols result;
+                result.push_back(tmp);
+                result.push_back(tmp);
+                return result;
+            })
+        .build();
+
+    // Main symbol
+    lsystem::Symbol symbolX =
+        lsystem::SymbolBuilder::getBuilder()
+        .setDrawingFunction(
+            [&] (lsystem::Symbol const &)
+            {
+                generator.drawLine();
+
+            })
+        .setProduction(
+            [=] (lsystem::Symbol const &s) -> lsystem::Symbols
+            {
+                lsystem::Symbol tmp(s);
+                lsystem::Symbols result;
+
+                result.push_back(symbolF);
+
+                result.push_back(lsystem::symbolSaveState);
+                result.push_back(lsystem::symbolYawLeft);
+                result.push_back(tmp);
+                result.push_back(lsystem::symbolRestoreState);
+
+                result.push_back(lsystem::symbolSaveState);
+                result.push_back(lsystem::symbolPitchUp);
+                result.push_back(tmp);
+                result.push_back(lsystem::symbolRestoreState);
+
+                result.push_back(symbolF);
+                result.push_back(lsystem::symbolSaveState);
+                result.push_back(lsystem::symbolYawRight);
+                result.push_back(tmp);
+                result.push_back(lsystem::symbolRestoreState);
+
+                result.push_back(lsystem::symbolSaveState);
+                result.push_back(lsystem::symbolPitchDown);
+                result.push_back(tmp);
+                result.push_back(lsystem::symbolRestoreState);
+
+                result.push_back(lsystem::symbolYawLeft);
+                result.push_back(tmp);
+                return result;
+            })
+        .build();
+
+    lsystem::Symbols axiom;
+    axiom.push_back(symbolX);
+    simulator.setAxiom(axiom);
+
+    simulator.setStepCount(4);
+    engine.addGraphicObject(
+            simulator.getGraphicObject(generator, 640 * 0.8, 480 * 0.8));
+
+
+    std::cout << "LSystem" << std::endl;
+    engine.start();
+}
+
 void simpleTree3b()
 {
     std::string vertexShaderFilename("../src/shaders/vertex_shader.vert");
@@ -469,7 +672,8 @@ void simpleTree3b()
                 result.push_back(lsystem::symbolSaveState);
                 //result.push_back(lsystem::symbolYawLeft);
                 result.push_back(lsystem::getSymbolYawRight(
-                            glm::half_pi< GLfloat >(), 0.2));
+                      glm::radians(glm::half_pi< GLfloat >())
+                    , glm::radians(glm::quarter_pi< GLfloat>() / 4.0)));
                 result.push_back(tmp);
                 result.push_back(symbolLateralBranch);
                 result.push_back(lsystem::symbolRestoreState);
@@ -520,106 +724,8 @@ void simpleTree3b()
 
 int main()
 {
-
-    // Test for lsystem::VertexGenerator
-    /*lsystem::VertexGenerator vertexGenerator;
-    vertexGenerator.setDrawState(std::make_tuple(glm::vec3(0, 0, 0), 0, 3.14 / 4));
-    vertexGenerator.setCommandsString("F-F+FF-F+FF-F+FF-F+F");
-    GraphicObjectPtr obj1 = vertexGenerator.generateGraphicObject(); 
-    engine.addGraphicObject(obj1);*/
-    
-
-    // Test for GraphicEngine
-    /*GraphicObjectPtr obj1(new GraphicObject);
-    obj1->addVertex(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    obj1->addVertex(glm::vec3(0.0f, 0.5f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    obj1->addVertex(glm::vec3(0.5f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    obj1->setDrawMode(GL_TRIANGLES);
-    engine.addGraphicObject(obj1);*/
-
-    // Test for LSystem generator
-    /*lsystem::Simulator simulator;
-    simulator.setAxiom("F-F-F-F");
-    simulator.setStartPoint(glm::vec3(0, 0, 0));
-    simulator.setDeltaAngle(glm::half_pi< GLfloat > ());
-    simulator.setStartAngle(0);
-
-    simulator.addProduction('F', "FF-F-F-F-F-F+F");
-    simulator.addProduction('+', "+");
-    simulator.addProduction('-', "-");
-    simulator.addProduction('f', "f");
-
-    simulator.addCommand('F', "F");
-    simulator.addCommand('+', "+");
-    simulator.addCommand('-', "-");
-    simulator.addCommand('f', "f");
-
-    simulator.setStepCount(3);
-    engine.addGraphicObject(simulator.getGraphicObject());*/
-
-    // Test for tree LSystem
-    /*lsystem::Simulator simulator;
-    simulator.setAxiom("F");
-    simulator.setStartPoint(glm::vec3(0, 0, 0));
-    simulator.setDeltaAngle(glm::quarter_pi< GLfloat > ());
-    simulator.setStartAngle(glm::half_pi< GLfloat >());
-
-    simulator.addProduction('F', "F[+F]F[-FF]F");
-    simulator.addProduction('+', "+");
-    simulator.addProduction('-', "-");
-    simulator.addProduction('[', "[");
-    simulator.addProduction(']', "]");
-
-    simulator.addCommand('F', "F");
-    simulator.addCommand('+', "+");
-    simulator.addCommand('-', "-");
-    simulator.addCommand('[', "[");
-    simulator.addCommand(']', "]");
-
-    simulator.setStepCount(4);
-    engine.addGraphicObject(simulator.getGraphicObject(400, 400));*/
-
-    // Test for SLSystem
-    /*lsystem::Simulator simulator;
-    simulator.setAxiom("SS");
-    simulator.setStartPoint(glm::vec3(0, 0, 0));
-    simulator.setDeltaAngle(glm::quarter_pi< GLfloat > ());
-    simulator.setStartAngle(glm::half_pi< GLfloat >());
-
-    simulator.addProduction('S', "SLSRS", 0.98);
-    simulator.addProduction('L', "[+SLS-SRS]", 0.75);
-    simulator.addProduction('R', "[-SLSSRS]", 0.85);
-    simulator.addProduction('+', "+");
-    simulator.addProduction('-', "-");
-    simulator.addProduction('[', "[");
-    simulator.addProduction(']', "]");
-
-    simulator.addCommand('S', "F");
-    simulator.addCommand('L', "[+F]");
-    simulator.addCommand('R', "[-FF]");
-    simulator.addCommand('+', "+");
-    simulator.addCommand('-', "-");
-    simulator.addCommand('[', "[");
-    simulator.addCommand(']', "]");*/
-
-    /*simulator.setAxiom("F");
-    simulator.setStartPoint(glm::vec3(0, 0, 0));
-    simulator.setDeltaAngle(glm::quarter_pi< GLfloat > ());
-    simulator.setStartAngle(glm::half_pi< GLfloat >());
-
-    simulator.addProduction('F', "F[+F]F[-F]F");
-    simulator.addProduction('+', "+");
-    simulator.addProduction('-', "-");
-    simulator.addProduction('[', "[");
-    simulator.addProduction(']', "]");
-
-    simulator.addCommand('F', "F");
-    simulator.addCommand('+', "+");
-    simulator.addCommand('-', "-");
-    simulator.addCommand('[', "[");
-    simulator.addCommand(']', "]");*/
-
-    //simpleTree2();
-    simpleTree3b();
+    simpleTree1b();
+    //simpleTree2b();
+    //simpleTree3b();
     return 0;
 }
